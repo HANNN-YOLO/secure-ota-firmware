2cc13070a2ef45e56f6b15fc8ea6ca96dd5f9897
HAN <reyhanrafaidhil1126@gmail.com>
2026-06-20 08:29:16 +0700

    repair path absolut

--- scripts/generate_hash.py
+++ scripts/generate_hash.py
@@ -1,6 +1,20 @@
-import hashlib
+# import hashlib
 
-with open("firmware/firmware.bin", "rb") as file:
+# with open("firmware/firmware.bin", "rb") as file:
+#     data = file.read()
+
+# sha256_hash = hashlib.sha256(data).hexdigest()
+
+# print("SHA256:", sha256_hash)
+
+import hashlib
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+FIRMWARE_PATH = BASE_DIR / "firmware" / "firmware.bin"
+
+with open(FIRMWARE_PATH, "rb") as file:
     data = file.read()
 
 sha256_hash = hashlib.sha256(data).hexdigest()
--- scripts/sign_firmware.py
+++ scripts/sign_firmware.py
@@ -1,12 +1,64 @@
+# import hashlib
+
+# from cryptography.hazmat.primitives import hashes
+# from cryptography.hazmat.primitives import serialization
+# from cryptography.hazmat.primitives.asymmetric import ec
+
+
+# # STEP 1 - Read firmware file
+# with open("../firmware/firmware.bin", "rb") as file:
+#     firmware_data = file.read()
+
+
+# # STEP 2 - Calculate SHA256 hash
+# firmware_hash = hashlib.sha256(firmware_data).digest()
+
+# print("Firmware Hash Generated Successfully")
+
+
+# # STEP 3 - Load private key
+# with open("../keys/private_key.pem", "rb") as key_file:
+#     private_key = serialization.load_pem_private_key(
+#         key_file.read(),
+#         password=None
+#     )
+
+# print("Private Key Loaded Successfully")
+
+
+# # STEP 4 - Sign hash using ECDSA
+# signature = private_key.sign(
+#     firmware_hash,
+#     ec.ECDSA(hashes.SHA256())
+# )
+
+# print("Digital Signature Generated Successfully")
+
+
+# # STEP 5 - Save signature
+# with open("../Output/signature.sig", "wb") as sig_file:
+#     sig_file.write(signature)
+
+# print("Signature saved to Output/signature.sig")
+
 import hashlib
+
+from pathlib import Path
 
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import ec
 
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+FIRMWARE_PATH = BASE_DIR / "firmware" / "firmware.bin"
+PRIVATE_KEY_PATH = BASE_DIR / "keys" / "private_key.pem"
+SIGNATURE_PATH = BASE_DIR / "output" / "signature.sig"
+
+
 # STEP 1 - Read firmware file
-with open("../firmware/firmware.bin", "rb") as file:
+with open(FIRMWARE_PATH, "rb") as file:
     firmware_data = file.read()
 
 
@@ -17,7 +69,7 @@ print("Firmware Hash Generated Successfully")
 
 
 # STEP 3 - Load private key
-with open("../keys/private_key.pem", "rb") as key_file:
+with open(PRIVATE_KEY_PATH, "rb") as key_file:
     private_key = serialization.load_pem_private_key(
         key_file.read(),
         password=None
@@ -36,7 +88,9 @@ print("Digital Signature Generated Successfully")
 
 
 # STEP 5 - Save signature
-with open("../Output/signature.sig", "wb") as sig_file:
+SIGNATURE_PATH.parent.mkdir(exist_ok=True)
+
+with open(SIGNATURE_PATH, "wb") as sig_file:
     sig_file.write(signature)
 
-print("Signature saved to Output/signature.sig")
+print("Signature saved to output/signature.sig")
